44223cc1ae23d9342209e76dabc1e13fa090ef01
Dexter Dwayne Tennison <83023757+Enduren@users.noreply.github.com>
2025-05-26 20:39:52 -0500

    removed comments and added new bgc and color to  .filter-dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,15 @@
 import React from "react";
 import "../styles.css";
 
-
 const Header = () => {
-    return (
-        <div className="header">
-            <img src="logo.png" alt="moviedux" className="logo" />
-            <h2 className="app-subtitle">It's time for popcorn! Find your next movie here. </h2>
-            
-        </div>
-    );
-}
+  return (
+    <div className="header">
+      <img src="logo.png" alt="moviedux" className="logo" />
+      <h2 className="app-subtitle">
+        It's time for popcorn! Find your next movie here.{" "}
+      </h2>
+    </div>
+  );
+};
 
 export default Header;
-
-// //rfc react functional component
-// import React from 'react'
-
-// const Header = () => {
-//     return (
-//         <div>
-            
-//         </div>
-//     )
-// }
-
-// export default 
-
-// //asf async function
-// async function (arguments) {
-    
-// }
-
-// //async arrow function
-// async (params) => {
-    
-// }
-// //nfn named function
-// const name = (params) => {
-    
-// }
